e9b4d21cb363b968306b28ee435cfc145c5ddb09
Gabriel Berlucci <gabrielberluccidev@proton.me>
2025-10-07 14:36:29 -0300

    feat: now return temp, city, icon and status from API

--- src/weather/getAPI.js
+++ src/weather/getAPI.js
@@ -2,18 +2,25 @@ import { API_KEY } from '../key';
 
 export async function getWeather(city) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_KEY}`;
-
   const response = await fetch(url);
 
-  try {
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+  if (!response.ok) {
+    if (response.status === 400 || response.status === 404) {
+      throw new Error('City not founded. Verify the name.');
     }
-
-    const result = await response.json();
-
-    return result.currentConditions.temp;
-  } catch (error) {
-    return error.message;
+    throw new Error(`Erro no servidor: ${response.status}`);
   }
+
+  const result = await response.json();
+
+  console.log(result);
+
+  const temp = result.currentConditions.temp;
+  const cityAPI = result.address;
+  const icon = result.currentConditions.icon;
+  const status = result.currentConditions.conditions;
+
+  console.log(temp, cityAPI, icon, status);
+
+  return { temp, cityAPI, icon, status };
 }
